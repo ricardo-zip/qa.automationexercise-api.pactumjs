e2e14edcfca30726db16b0d068aae04bf9e2b508
Ricardo Dias <90585892+ricardo-zip@users.noreply.github.com>
2025-06-14 21:47:20 +0000

    start functional tests

--- tests/functional/test.js
+++ tests/functional/test.js
@@ -1,8 +1,24 @@
-// test.js
-const { spec } = require('pactum');
+const { spec } = require("pactum");
+const { setup } = require("../../config/pactum.config");
+const { createValidUser } = require("../../utils/create.user.helpers");
 
-it('should get a response with status code 200', async () => {
-  await spec()
-    .get('http://httpbin.org/status/200')
+beforeEach(async () => {
+  testUser = createValidUser();
+
+  await pactum
+    .spec()
+    .post("/createAccount")
+    .withForm(testUser)
     .expectStatus(200);
+});
+
+describe("API 11: Validar a criação de um novo registro de Usuário", () => {
+  it("CT01 - Criar usuário com dados válidos - Deve retornar 201 Created e confirmar criação com ID", async () => {
+    await pactum
+      .spec()
+      .post("/createAccount")
+      .withForm(testUser)
+      .expectStatus(201)
+      .expectJsonLike({ responseCode: 201 });
+  });
 });
